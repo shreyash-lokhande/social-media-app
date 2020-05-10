3d9f945a4752557ca0765cdcc34fe5f7cd731571
Guney Ural <guneyural@gmail.com>
2020-05-11 00:22:47 +0300

    add user configuration for email operations

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const moment = require('moment');
 const Comment = require('./comment');
 const Post = require('./post');
 
@@ -10,7 +11,15 @@ const userSchema = new mongoose.Schema({
     profilePicture: {type: String, default: ''},
     bio: {type: String, default: ''},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    operations: {
+        operationType: {
+            type: String,
+            enum: ["REMOVE_ACCOUNT", "FORGOT_CREDENTIALS"]
+        },
+        expiresAt: {type: Date},
+        operationId: {type: String}
+    }
 }, { timestamps: true });
 
 userSchema.pre('save', async function(next) {
@@ -41,6 +50,23 @@ userSchema.methods.comparePassword = async function(password, cb) {
     try {
         const compare = await bcrypt.compare(password, this.password);
         return cb(null, compare);
+    } catch(err) {
+        return cb(err, null);
+    }
+}
+
+userSchema.methods.addOperation = async function(opType, cb) {
+    try {
+        let text = `${opType}+${Math.random()}+${Date.now()}`;
+        const salt = await bcrypt.genSalt(5);
+        const operationId = await bcrypt.hash(text, salt);
+        const expiresAt = moment(moment().format()).add(5, 'm').toDate();
+
+        this.operations.operationType = opType.toUpperCase();;
+        this.operations.operationId = operationId;
+        this.operations.expiresAt = expiresAt;
+
+        return cb(null, {operationType: opType, msg: '5 minutes to perform this action.' });
     } catch(err) {
         return cb(err, null);
     }
