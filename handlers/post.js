5cb528afbf3e616a051de2695fd7128583f7a07c
Guney Ural <guneyural@gmail.com>
2020-05-11 00:22:15 +0300

    add handler functions to fill later

---
+++ handlers/post.js
@@ -0,0 +1,71 @@
+const Config = require('../config');
+const Validations = require('../validations');
+const User = require('../models/user');
+const Post = require('../models/post');
+const PostLike = require('../models/postLike');
+const Comment = require('../models/comment');
+const Followers = require('../models/followers');
+const Followings = require('../models/followings');
+
+const addPost = async (req, res) => {
+
+}
+
+const getPosts = async (req, res) => {
+
+}
+
+const getPostById = async (req, res) => {
+
+}
+
+const searchPost = async (req, res) => {
+
+}
+
+const updatePost = async (req, res) => {
+
+}
+
+const removePost = async (req, res) => {
+
+}
+
+const addComment = async (req, res) => {
+    
+}
+
+const updateComment = async (req, res) => {
+
+}
+
+const removeComment = async (req, res) => {
+
+}
+
+const likePost = async (req, res) => {
+
+}
+
+const unlikePost = async (req, res) => {
+
+}
+
+const getPostLikes = async (req, res) => {
+
+}
+
+module.exports = {
+    addPost,
+    getPosts,
+    getPostById,
+    searchPost,
+    updatePost,
+    removePost,
+    addComment,
+    updateComment,
+    removeComment,
+    likePost,
+    unlikePost,
+    getPostLikes
+}
--- handlers/user.js
+++ handlers/user.js
@@ -1,12 +1,101 @@
 const Config = require('../config');
-const User = require('../models/user');
+const Validations = require('../validations');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const Notification = require('../models/notification');
+const Followers = require('../models/followers');
+const Followings = require('../models/followings');
 
-const uploadPhoto = async (req, res) => {
+const uploadProfilePhoto = async (req, res) => {
     console.log(req.file.path);
 }
 
+const changeProfilePicture = async (req, res) => {
+
+}
+
+const removeProfilePicture = async (req, res) => {
+
+}
+
+const register = async (req, res) => {
+
+}
+
+const resetPassword = async (req, res) => {
+
+}
+
+const forgotPassword = async (req, res) => {
+
+}
+
+const login = async (req, res) => {
+
+}
+
+const removeUser = async (req, res) => {
+
+}
+
+const updateUserData = async (req, res) => {
+
+}
+
+const getUser = async (req, res) => {
+
+}
+
+const followUser = async (req, res) => {
+
+}
+
+const getNewUsers = async (req, res) => {
+
+}
+
+const getUserById = async (req, res) => {
+
+}
+
+const searchUser = async (req, res) => {
+
+}
+
+const getFollowings = async (req, res) => {
+
+}
+
+const getFollowers = async (req, res) => {
+
+}
+
+const unfollowUser = async (req, res) => {
+
+}
+
+const getNotifications = async (req, res) => {
+
+}
+
 module.exports = {
-    uploadPhoto
+    uploadProfilePhoto,
+    changeProfilePicture,
+    removeProfilePicture,
+    register,
+    resetPassword,
+    forgotPassword,
+    login,
+    removeUser,
+    updateUserData,
+    getUser,
+    followUser,
+    getNewUsers,
+    getUserById,
+    searchUser,
+    getFollowings,
+    getFollowers,
+    unfollowUser,
+    getNotifications
 }
