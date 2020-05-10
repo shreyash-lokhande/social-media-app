63e966d7e3e06e0291793b9c5ff54bbaec8ecfe5
Guney Ural <guneyural@gmail.com>
2020-05-11 00:19:51 +0300

    add validation functions to fill later

---
+++ validations/index.js
@@ -0,0 +1,24 @@
+const Joi = require('@hapi/joi');
+
+const registerValidation = data => {
+
+}
+
+const loginValidation = data => {
+
+}
+
+const postValidation = data => {
+
+}
+
+const commentValidation = data => {
+
+}
+
+module.exports = {
+    registerValidation,
+    loginValidation,
+    postValidation,
+    commentValidation
+}
