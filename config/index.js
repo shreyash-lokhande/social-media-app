272bd20b0b73e1dedbadeddc4ed7ae292762e96a
Guney Ural <guneyural@gmail.com>
2020-05-12 02:54:07 +0300

    add nodemailer configuration

--- config/index.js
+++ config/index.js
@@ -1,4 +1,13 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGOURI: ""
+    MONGOURI: "mongodb+srv://guneyral:guneyguney@guneyural-boaqq.mongodb.net/test?retryWrites=true&w=majority",
+    SECRET: 'SHHHHH',
+    DEFAULT_PROFILE_PICTURE: "uploads/1589234862373nopic.png",
+    TRANSPORTER: {
+        service: 'gmail',
+        auth: {
+            user: 'ihtiyarbebe497@gmail.com',
+            pass: 'myPassword1'
+        }
+    }
 };
